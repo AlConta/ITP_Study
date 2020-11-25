3f561eb7acf71c13110ca5f3554620e7d79f1c79
AlConta <umazaemon@gmail.com>
2020-11-25 11:56:47 +0900

    ITP学習進捗更新

---
+++ ITP/ITP_1_8_D/ITP_1_8_D.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define Rep(i, a, b) for(int i = a; i <= b; i++)
+#define sz(s) (int)s.size()
+
+int main() {
+    string s, p;
+    cin >> s >> p;
+    int ringSize = sz(s);
+    int ansSize = sz(p);
+    s += s;
+    rep(i, 0, ringSize) {
+        if (s.substr(i, ansSize) == p) {
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+
+    cout << "No" << endl;
+    return 0;
+}
